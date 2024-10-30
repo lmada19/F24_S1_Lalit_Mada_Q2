6ed0d08862eabc12dd55d5c9743b8384eb8f028d
lmada <lmada@uoguelph.ca>
2024-10-30 16:36:01 -0400

    Handle null or empty string inputs in reverse methods

--- src/com/jwetherell/algorithms/strings/StringFunctions.java
+++ src/com/jwetherell/algorithms/strings/StringFunctions.java
@@ -13,20 +13,29 @@ public class StringFunctions {
     private static final char SPACE = ' ';
 
     public static final String reverseWithStringConcat(String string) {
-        String output = new String();
-        for (int i = (string.length() - 1); i >= 0; i--) {
-            output += (string.charAt(i));
-        }
-        return output;
-    }
+        if (string == null || string.isEmpty()) {
+        	return ""; // Return an empty string if it is
+    	}
+
+    	String output = new String();
+    	for (int i = (string.length() - 1); i >= 0; i--) {
+        	output += (string.charAt(i));
+    	}
+    	return output;
+	}
 
     public static final String reverseWithStringBuilder(String string) {
-        final StringBuilder builder = new StringBuilder();
-        for (int i = (string.length() - 1); i >= 0; i--) {
-            builder.append(string.charAt(i));
-        }
-        return builder.toString();
-    }
+        if (string == null || string.isEmpty()) {
+        	return ""; // Return an empty string if it is
+    	}
+
+    	final StringBuilder builder = new StringBuilder();
+    	for (int i = (string.length() - 1); i >= 0; i--) {
+        	builder.append(string.charAt(i));
+    	}
+    	return builder.toString();   
+
+	}
 
     public static final String reverseWithStringBuilderBuiltinMethod(String string) {
         final StringBuilder builder = new StringBuilder(string);
